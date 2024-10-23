341e4409d654fd7074d7aa39d840a30abf5e0d73
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-10-23 13:55:49 +0000

    Fix issue with RTT

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 extern crate cortex_m;
 
+use cortex_m::asm::nop;
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
@@ -13,6 +14,9 @@ fn main() -> ! {
     rtt_init_print!();
     rprintln!("Setting Up");
     loop {
-        rprintln!("Hey?")
+        rprintln!("Hey?");
+        for _ in 0..100_000 {
+            nop();
+        }
     }
 }
